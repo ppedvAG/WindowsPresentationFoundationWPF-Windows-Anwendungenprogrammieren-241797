d997938e8d550f90092de3b20906ff6cbc2e3151
ppedvLukasK <lukask@ppedv.de>
2024-05-02 11:38:48 +0200

    M005

---
+++ M005/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace M005;
+
+public partial class MainWindow : Window
+{
+	//public Person person { get; set; } // = new Person(); // = new Person() { Vorname = "Max", Nachname = "Mustermann", Alter = 33 };
+
+	public BindableProperty<Person> person { get; set; } = new();
+
+	/// <summary>
+	/// Funktioniert wie List, aber benachrichtigt die GUI, wenn ein Element hinzugefügt oder entfernt wird
+	/// </summary>
+	public ObservableCollection<Person> personen { get; set; } = new ObservableCollection<Person>();
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		//Dieser Personen Datensatz soll an die GUI gebunden werden
+		person.Value = new Person(); //Die person Variable hat selbst keinen Benachrichtigungsmechanismus -> hier müsste auch INotifyPropertyChanged implementiert werden
+		person.Value.Vorname = "Max"; //Hier findet eine Benachrichtigung statt
+		person.Value.Nachname = "Mustermann"; //Hier findet eine Benachrichtigung statt
+		person.Value.Alter = 33; //Hier findet eine Benachrichtigung statt
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		person.Value.Alter++;
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		personen.Add(new Person() { Alter = Random.Shared.Next() });
+	}
+}
