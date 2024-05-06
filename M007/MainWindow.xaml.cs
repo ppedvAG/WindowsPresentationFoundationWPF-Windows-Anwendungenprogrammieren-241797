64ee005a738365b2d43a6c81be3ffdae16f2d7bb
ppedvLukasK <lukask@ppedv.de>
2024-05-06 09:42:07 +0200

    M007

---
+++ M007/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace M007;
+
+public partial class MainWindow : Window
+{
+	public ObservableCollection<Person> personen { get; set; } = new();
+
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		personen.Add(new Person() { Vorname = "Max", Nachname = "Mustermann", Alter = Random.Shared.Next() });
+	}
+}
---
+++ M007/Person.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace M007;
+
+public class Person : INotifyPropertyChanged
+{
+	private string vorname;
+
+	public string Vorname
+	{
+		get => vorname;
+		set
+		{
+			vorname = value;
+			Notify(nameof(Vorname));
+			//Notify("Vorname");
+		}
+	}
+
+	private string nachname;
+
+	public string Nachname
+	{
+		get => nachname;
+		set
+		{
+			nachname = value;
+			Notify(nameof(Nachname));
+		}
+	}
+
+	private int alter;
+
+	public int Alter
+	{
+		get => alter;
+		set
+		{
+			alter = value;
+			Notify(nameof(Alter));
+		}
+	}
+
+	//Die GUI hängt dieses Event automatisch an, wir müssen es feuern
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	//Diese Methode muss jetzt bei jedem Property (Vorname, Nachname und Alter) implementiert werden, damit diese Properties benachrichtigen können
+	public void Notify(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
